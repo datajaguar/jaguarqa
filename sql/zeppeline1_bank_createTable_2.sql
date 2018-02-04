f7f5f4666eba929621b91a2714f868e7b426f63d
andrew <andrew@dell3.(none)>
2018-02-04 14:30:47 -0800

    update

---
+++ sql/zeppeline1_bank_createTable_2.sql
@@ -0,0 +1,36 @@
+
+drop table if exists zeppeline1_bank_2;
+
+
+spool $QA_HOME/work/zeppeline1_bank_creatTable_2.out;
+
+create table zeppeline1_bank_2 
+(
+    key:
+        id uuid,
+    Value:
+        age int,
+        job varchar (50),
+        marital varchar (10),
+        education varchar (50),
+        default_1 varchar (3),
+        balance int,
+        housing varchar (10),
+        loan varchar (10),
+        contact varchar (10),
+        day int,
+        month varchar (10),
+        duration int,
+        campaign int,
+        pdays int,
+        previous int,
+        poutcome varchar (10),
+        y varchar (10) 
+);
+
+desc zeppeline1_bank_2;
+
+spool off;
+quit;
+
+
---
+++ sql/zeppeline1_bank_loadData_2.sql
@@ -0,0 +1,4 @@
+# Script Name: zeppeline1_bank_loadData_2.sql 
+
+load $QA_HOME/data/bank-full.csv into zeppeline1_bank_2;
+
