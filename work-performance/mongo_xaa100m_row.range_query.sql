0bc354dc8fabe3fd78f30dac4cb64b93060f7bf6
agz1117 <agz1117@hotmail.com>
2018-03-27 20:46:30 -0700

    update

--- work-performance/mongo_xaa100m_row.range_query.sql
+++ work-performance/mongo_xaa100m_row.range_query.sql
@@ -1,7 +1,12 @@
 
 set verbose true;
 use jagdb;
-db.xaa100m_row.find({uid:  { $lt: '9d%', $gt: '3d'} }).count()
+
+# db.xaa100m_row.find({uid:  { $lt: '9d%', $gt: '3d'} }).count()  # 4 sec
+# db.xaa100m_row.find({uid:  { $gt: 'd%'} }).count()   # 30 sec
+db.xaa100m_row.find({uid:  { $lt: '9d%'} }).count()  # 5 sec
+
+db.xaa100m_row.find({uid:  { $lt: 'd%'} }).count()
 
 exit;
 
